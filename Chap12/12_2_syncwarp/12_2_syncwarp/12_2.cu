5648ee50a4b45bc020ebdf9181e02ae7f26a0ad0
HongJu Jeong <sub06038@naver.com>
2023-07-21 16:39:12 +0900

    syncwarp

---
+++ Chap12/12_2_syncwarp/12_2_syncwarp/12_2.cu
@@ -0,0 +1,26 @@
+﻿
+#include "cuda_runtime.h"
+#include "device_launch_parameters.h"
+
+#include <stdio.h>
+
+#define BLOCK_SIZE 64
+
+__global__ void syncWarp_test()
+{
+	int tID = threadIdx.x;
+	int warpID = (int)(tID / 32);
+	__shared__ int masterID[BLOCK_SIZE / 32];
+	
+	if (threadIdx.x % 32 == 0) {
+		masterID[warpID] = tID;
+	}
+	//__syncwarp();
+
+	printf("[T%d] The master of our warp is %d\n", tID, masterID[warpID]);
+}
+
+int main()
+{
+	syncWarp_test << <1, BLOCK_SIZE >> > ();
+}
